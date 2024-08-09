504394cd9f26af9a6143de24efec28f76e9c245b
mariahlmiranda <mariahlmiranda@gmail.com>
2024-08-08 21:37:07 -0300

    commit 2

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
 const numeroUm = document.getElementById('numero-um'); 
 const numeroDois = document.getElementById('numero-dois');
-const mensagemSucesso = `Sucesso o número:<b>${numeroDois.value}</b> é maior que:<b>${numeroUm.value}</b> `;
-const mensagemErro = `Erro o número: <b>${numeroDois.value} </b> é maior que <b>${numeroUm.value}</b>`;
+const mensagemSucesso = `Sucesso! O número do primeiro campo é menor do que o do segundo. Essa foi por pouco! (ﾉ◕ヮ◕)ﾉ*:･ﾟ`;
+const mensagemErro = `Erro: o número do primeiro campo deve ser menor do que o do segundo campo. (Alguém está indo até você)`;
 
 if (Number(numeroDois.value) > Number(numeroUm.value)) {
     const containerMensagemSucesso = document.querySelector('.success-message');
@@ -18,7 +18,7 @@ numeroUm.value = '';
 numeroDois.value = '';
 
 } else {
-    numeroDois.style.border = '1px solid red';
+    numeroDois.style.border = '1px solid #c75656';
     document.querySelector ('.error-message').style.display = 'block';
     const containerMensagemErro = document.querySelector('.error-message');
     containerMensagemErro.innerHTML = mensagemErro;
